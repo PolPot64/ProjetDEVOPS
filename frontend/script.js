ed60f9b53749d4b023dbea64e9f7ebc027a47ae0
Paul Baudéan <63459617+PolPot64@users.noreply.github.com>
2025-02-10 12:34:25 +0100

    bug JS

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,14 @@
 function fetchData() {
-    fetch('http://localhost:8080/api-0.0.1/api/openapi.json')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('data').innerText = JSON.stringify(data);
-        })
-        .catch(error => console.error('Error:', error));
+    fetch('http://localhost:8080/api-0.0.1/api/Bids', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer test'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('data').innerText = JSON.stringify(data, null, 2);
+    })
+    .catch(error => console.error('Error:', error));
 }
